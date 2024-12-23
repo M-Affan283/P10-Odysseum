4e5406c9e85a8e55ed53a95fa711ed7f19196008
Muhammad Affan <25100283@lums.edu.pk>
2024-12-23 21:30:31 +0500

    Added backend for adding and getting locations

--- Development/Sprint-1/Code/server/controllers/LocationController/getLocation.js
+++ Development/Sprint-1/Code/server/controllers/LocationController/getLocation.js
@@ -5,13 +5,37 @@
 import { Location } from "../../models/Location.js";
 import { ERROR_MESSAGES } from "../../utils/constants.js";
 
+
+/**
+ * Get all locations.
+ * @param {Object} req - Request object.
+ * @param {Object} res - Response object.
+ * @returns {Object} - Response object.
+ */
+const getAllLocations = async (req,res) =>
+{
+    try
+    {
+        const locations = await Location.find({});
+        return res.status(200).json({message: SUCCESS_MESSAGES.USERS_FOUND, locations: locations});
+
+    }
+    catch(error)
+    {
+        console.log(error);
+        return res.status(500).json({error: ERROR_MESSAGES.SERVER_ERROR});
+    }
+
+}
+
 /**
  * Search for locations by name or description.
  * @param {Object} req - Request object containing the search parameters.
  * @param {Object} res - Response object.
  */
 
-export const searchLocations = async (req, res) => {
+const searchLocations = async (req, res) => 
+{
 
   const { searchParam="", limit = 5, lastId } = req.query;
 
@@ -48,3 +72,24 @@ export const searchLocations = async (req, res) => {
     return res.status(500).json({ error: ERROR_MESSAGES.SERVER_ERROR });
   }
 };
+
+const getLocationById = async (req, res) =>
+{
+  const { locationId } = req.query;
+
+  try
+  {
+    const location = await Location.findById(locationId);
+
+    if(!location) return res.status(404).json({error: ERROR_MESSAGES.LOCATION_NOT_FOUND});
+
+    return res.status(200).json({location: location});
+  }
+  catch(error)
+  {
+    console.log(error);
+    return res.status(500).json({error: ERROR_MESSAGES.SERVER_ERROR});
+  }
+}
+
+export { getAllLocations, searchLocations, getLocationById };
