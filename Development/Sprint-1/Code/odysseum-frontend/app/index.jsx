15a2929bcb8cbb74f4c84e49cab1681ea0892ab6
Muhammad Affan <25100283@lums.edu.pk>
2024-12-26 19:44:55 +0500

    Added single post details, comments and image carousel

--- Development/Sprint-1/Code/odysseum-frontend/app/index.jsx
+++ Development/Sprint-1/Code/odysseum-frontend/app/index.jsx
@@ -1,15 +1,29 @@
+import React from "react";
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { Text, View, TouchableOpacity, ScrollView, Image } from "react-native";
+import { Text, View, TouchableOpacity, ScrollView, Image, Dimensions } from "react-native";
 import WelcomeScreen from "../src/screens/WelcomeScreen";
 import UserProfileScreen from "../src/screens/UserProfileScreen";
+import HomeScreen from "../src/screens/HomeScreen";
+import CreatePostScreen from "../src/screens/CreatePostScreen";
+import BookmarkScreen from "../src/screens/BookmarkScreen";
+import PostDetailsScreen from "../src/screens/PostDetailsScreen";
+import DiscoverLocationsScreen from "../src/screens/DiscoverLocationsScreen";
+import PostCard from "../src/components/PostCard";
+
+
+// Component testing
 
 export default function App() {
   return (
     <>
-      <WelcomeScreen />
-      {/* <UserProfileScreen /> */}
-      <StatusBar backgroundColor="#161622" style="light" />
+      {/* <WelcomeScreen /> */}
+      {/* <HomeScreen /> */}
+      {/* <BookmarkScreen /> */}
+      {/* <CreatePostScreen /> */}
+      <UserProfileScreen />
+      {/* <PostDetailsScreen postId="6730787a070ca3617028ad30" /> */}
+      {/* <DiscoverLocationsScreen /> */}
     </>
   );
 }
