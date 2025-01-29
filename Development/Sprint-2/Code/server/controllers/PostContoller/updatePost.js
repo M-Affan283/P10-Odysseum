0b8ab797bf9c56eea819b1a4d6d00b71978ff0c3
Muhammad Affan <25100283@lums.edu.pk>
2025-01-29 21:38:38 +0500

    Added like post

--- Development/Sprint-2/Code/server/controllers/PostContoller/updatePost.js
+++ Development/Sprint-2/Code/server/controllers/PostContoller/updatePost.js
@@ -15,11 +15,8 @@ import { SUCCESS_MESSAGES,ERROR_MESSAGES } from "../../utils/constants.js";
 
 /**
  * Updates a post in the database
- * @param {Object} req - request object. Must contain postId, requestorId, and newContent.
- * @param {Object} res - response object
- * @returns {Object} - response object
  */
-export const updatePost = async (req, res) =>
+const updatePost = async (req, res) =>
 {
     const { postId, requestorId, newContent } = req.body;
 
@@ -48,3 +45,43 @@ export const updatePost = async (req, res) =>
         return res.status(500).json({ message: ERROR_MESSAGES.SERVER_ERROR });
     }
 }
+
+/**
+ * Like/Unlike a post
+ */
+const likePost = async (req, res) => 
+{
+    const { postId, userId } = req.body;
+
+    if (!postId || !userId) return res.status(400).json({ message: ERROR_MESSAGES.MISSING_FIELDS });
+
+    try
+    {
+        const user = await User.findById(userId);
+        if(!user) return res.status(404).json({ message: ERROR_MESSAGES.USER_NOT_FOUND });
+
+        const post = await Post.findById(postId);
+        if(!post) return res.status(404).json({ message: ERROR_MESSAGES.POST_NOT_FOUND });
+
+        if(post.likes.includes(userId))
+        {
+            post.likes = post.likes.filter(like => like.toString() !== userId);
+            await post.save();
+            return res.status(200).json({ message: "Unliked" });
+        }
+        else
+        {
+            post.likes.push(userId);
+            await post.save();
+            return res.status(200).json({ message: "Liked" });
+        }
+
+    }
+    catch(error)
+    {
+        console.log(error);
+        return res.status(500).json({ message: ERROR_MESSAGES.SERVER_ERROR });
+    }
+}
+
+export { updatePost, likePost };
