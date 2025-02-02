a219cc4bf4a593768a33f68b8203e7f4401e7573
Muhammad Affan <25100283@lums.edu.pk>
2025-02-02 16:52:38 +0500

    Added new assets

---
+++ Development/Sprint-2/Code/odysseum-frontend/assets/icons/icons.js
@@ -0,0 +1,31 @@
+import Dining from './dining.png';
+import Hotel from './hotel.png';
+import Shopping from './shopping.png';
+import Fitness from './fitness.png';
+import Health from './health.png';
+import Beauty from './beauty.png';
+import History from './history.png';
+import Star from './star.png';
+import Accessiblity from './accessibility.png';
+import Celebration from './celebration.png';
+import Category from './category.png';
+import Business from './business.png';
+import Explore from './explore.png';
+
+const icons = {
+    dining: Dining,
+    hotel: Hotel,
+    shopping: Shopping,
+    fitness: Fitness,
+    health: Health,
+    beauty: Beauty,
+    history: History,
+    star: Star,
+    accessibility: Accessiblity,
+    celebration: Celebration,
+    category: Category,
+    business: Business,
+    explore: Explore
+}
+
+export default icons;
