e54cc224c4916fd12c15b04ce84445aeb6d1b003
Muhammad Affan <25100283@lums.edu.pk>
2025-02-02 16:39:55 +0500

    Paginated with tanstack

--- Development/Sprint-2/Code/odysseum-frontend/src/screens/ReviewsScreen.jsx
+++ Development/Sprint-2/Code/odysseum-frontend/src/screens/ReviewsScreen.jsx
@@ -1,5 +1,5 @@
-import { View, Text, Image, TouchableOpacity, FlatList } from "react-native";
-import React from "react";
+import { View, Text, Image, TouchableOpacity, FlatList, ActivityIndicator } from "react-native";
+import React, { useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StarRatingDisplay } from 'react-native-star-rating-widget';
 import {ShareIcon, ChevronLeftIcon } from "react-native-heroicons/outline";
@@ -8,6 +8,8 @@ import { calculateDuration } from "../utils/dateTimCalc";
 import { router } from "expo-router";
 import AddReviewModal from "../components/AddReviewModal";
 import axiosInstance from "../utils/axios";
+import useUserStore from "../context/userStore";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 const tempReviews = [
   {
@@ -26,21 +28,21 @@ const tempReviews = [
     "title": "Test Review",
     "reviewContent": "This is some test review content.",
     "imageUrls": [],
-    "upvotes": 0,
-    "downvotes": 0,
+    "upvotes": [],
+    "downvotes": [],
     "createdAt": "2025-01-10T15:13:55.665Z",
     "updatedAt": "2025-01-10T15:13:55.665Z",
     "__v": 0
   },
 ]
 
-const getQueryReviews = async ({ entityType, entityId, pageParam = 1 }) =>
+const getQueryReviews = async ({ entityType, entityId, pageParam = 1, requestorId }) =>
 {
-  console.log(pageParam);
+  // console.log(pageParam);
 
   try
   {
-    const res = await axiosInstance.get(`/review/getByEntity?page=${pageParam}&entityType=${entityType}&entityId=${entityId}`);
+    const res = await axiosInstance.get(`/review/getByEntity?page=${pageParam}&entityType=${entityType}&entityId=${entityId}&requestorId=${requestorId}`);
     // console.log(res.data);
     return res.data;
   }
@@ -53,15 +55,65 @@ const getQueryReviews = async ({ entityType, entityId, pageParam = 1 }) =>
 
 const ReviewsScreen = ({ entityType, entityId, entityName }) => {
 
-  const [reviews, setReviews] = React.useState(tempReviews || null);
+  // const [reviews, setReviews] = React.useState(tempReviews || null);
   const [addReviewModalVisible, setAddReviewModalVisible] = React.useState(false);
 
-  const upvoteReview = (reviewId) => {}
-  const downvoteReview = (reviewId) => {}
+  const user = useUserStore((state) => state.user);
+
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isFetching, error, refetch } = useInfiniteQuery({
+    queryKey: ["reviews", { entityType, entityId, requestorId: user._id }],
+    queryFn: ({ pageParam = 1 }) => getQueryReviews({ entityType, entityId, pageParam, requestorId: user._id }),
+    getNextPageParam: (lastPage) => {
+      const { currentPage, totalPages } = lastPage;
+      return currentPage < totalPages ? currentPage + 1 : undefined;
+    },
+    enabled: true,
+  });
+
+  const reviews = data?.pages.flatMap((page) => page.reviews) || tempReviews;
+
+
+  const loadMoreReviews = () =>
+  {
+    if(hasNextPage) fetchNextPage();
+  }
+
+  const handleRefresh = async () =>
+  {
+    await refetch();
+  }
+
+
+  const upvoteReview = (reviewId) =>
+  {
+    console.log("Upvoting: ", reviewId);
+
+    axiosInstance.post("/review/upvote", { reviewId, userId: user._id })
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+
+
+  }
+  const downvoteReview = (reviewId) =>
+  {
+    console.log("Downvoting: ", reviewId);
+
+    axiosInstance.post("/review/downvote", { reviewId, userId: user._id })
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
 
   const ListHeaderComponent = () => {
     return (
-      <View>
+      <View className="mt-2">
         <View className="flex-row items-center mb-5 gap-5">
 
           <TouchableOpacity onPress={() => router.back()} className="py-4">
@@ -79,10 +131,48 @@ const ReviewsScreen = ({ entityType, entityId, entityName }) => {
     )
   }
 
+  const ListEmptyComponent = () =>
+  {
+    if (isFetching) 
+    {
+      return (
+        <View className="flex-1 mt-5 justify-center items-center">
+          <ActivityIndicator size="large" color="black" />
+          <Text className="mt-3 text-gray-500">Loading reviews...</Text>
+        </View>
+      );
+    }
+    else if (error) 
+    {
+      return (
+        <View className="flex-1 mt-5 justify-center items-center">
+          <Text className="text-lg text-red-500">Failed to fetch reviews.</Text>
+          <Text className="text-lg text-red-500">{error.message}</Text>
+          <TouchableOpacity
+            className="mt-3 bg-blue-500 py-2 px-4 rounded-full"
+            onPress={handleRefresh}
+          >
+            <Text className="text-white">Retry</Text>
+          </TouchableOpacity>
+        </View>
+      );
+    }
+    else if (reviews?.length === 0)
+    {
+      return (
+        <View className="flex-1 mt-5 justify-center items-center">
+          <Text className="text-lg text-gray-500">No reviews found</Text>
+        </View>
+      );
+
+    }
+    return null;
+  }
+
   const renderItem = ({item}) => {
     return (
       <View className="mx-3 mt-5">
-        <ReviewCard review={item} upvote={upvoteReview} downvote={downvoteReview} />
+        <ReviewCard review={item} upvote={upvoteReview} downvote={downvoteReview} userId={user._id} />
       </View>
     )
   }
@@ -94,11 +184,12 @@ const ReviewsScreen = ({ entityType, entityId, entityName }) => {
           data={reviews}
           keyExtractor={(item) => item._id}
           contentContainerStyle={{paddingBottom: 50, gap:20}}
-          // onEndReached={}
-          // onEndReachedThreshold={}
-          // refreshing
-          // onRefresh={}
+          onEndReached={loadMoreReviews}
+          onEndReachedThreshold={0.5}
+          refreshing={isFetching && !isFetchingNextPage}
+          onRefresh={handleRefresh}
           ListHeaderComponent={ListHeaderComponent}
+          ListEmptyComponent={ListEmptyComponent}
           renderItem={renderItem}
         
         />
@@ -109,11 +200,44 @@ const ReviewsScreen = ({ entityType, entityId, entityName }) => {
   );
 };
 
-const ReviewCard = ({ review, upvote, downvote }) => {
+const ReviewCard = ({ review, upvote, downvote, userId }) => {
 
   const [showMore, setShowMore] = React.useState(false); // change this to true if lines exceed 2
   const [upvoted, setUpvoted] = React.useState(false);
   const [downvoted, setDownvoted] = React.useState(false);
+
+  useEffect(() => {
+    if (review?.upvotes?.includes(userId)) setUpvoted(true); setDownvoted(false);
+    if (review?.downvotes?.includes(userId)) setDownvoted(true); setUpvoted(false);
+  }, [review]);
+
+
+  //WARNING: Upvote and downvote are unstable and untested so don't use them
+  const handleUpvote = () => {
+    if (upvoted) setUpvoted(false);
+    else 
+    {
+      // upvote
+      setUpvoted(true);
+      setDownvoted(false);
+    }
+
+    // call upvote API
+    // upvote(review._id);
+  }
+
+  const handleDownvote = () => {
+    if (downvoted) setDownvoted(false);
+    else
+    {
+      // downvote
+      setUpvoted(false);
+      setDownvoted(true);
+    }
+
+    // call downvote API
+    // downvote(review._id);
+  }
 
   const CardFooter = () => {
     return (
@@ -121,16 +245,20 @@ const ReviewCard = ({ review, upvote, downvote }) => {
           <View className="flex-row justify-between items-center mt-2 px-2">
 
               <View>
-                <HandThumbUpIcon size={20} color="gray" />
-                <Text style={{color: "gray", paddingHorizontal: 5}}>{review?.upvotes}</Text>
+                <TouchableOpacity onPress={handleUpvote}>
+                  <HandThumbUpIcon size={30} color={upvoted ? "blue" : "gray"} />
+                  <Text className="text-gray-500 mx-auto">{review?.upvotes?.length}</Text>
+                </TouchableOpacity>
               </View>
 
               <View>
-                <HandThumbDownIcon size={20} color="gray" />
-                <Text style={{color: "gray", paddingHorizontal: 5}}>{review?.downvotes}</Text>
+                <TouchableOpacity onPress={handleDownvote}>
+                  <HandThumbDownIcon size={30} color={downvoted ? "blue" : "gray"} />
+                  <Text className="text-gray-500 mx-auto">{review?.downvotes?.length}</Text>
+                </TouchableOpacity>
               </View>
 
-              <ShareIcon size={20} color="gray" />
+              <ShareIcon size={30} color="gray" />
               
           </View>
       </View>
