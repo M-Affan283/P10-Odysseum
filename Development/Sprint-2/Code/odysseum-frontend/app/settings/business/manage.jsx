25b13d41c6e9928512586c007ba75a8ce6ece435
Muhammad Affan <25100283@lums.edu.pk>
2025-02-02 16:40:38 +0500

    Added business page in settings

---
+++ Development/Sprint-2/Code/odysseum-frontend/app/settings/business/manage.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import BusinessManageScreen from '../../../src/screens/BusinessManageScreen'
+
+const ManageBusiness = () => {
+  return (
+    <BusinessManageScreen />
+  )
+}
+
+export default ManageBusiness
