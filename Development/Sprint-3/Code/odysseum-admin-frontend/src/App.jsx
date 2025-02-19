2a313fb167fe338477d667de4358d40b95167a70
Muhammad Affan <25100283@lums.edu.pk>
2025-02-19 21:44:12 +0500

    Setup tailwind and antdesign

--- Development/Sprint-3/Code/odysseum-admin-frontend/src/App.jsx
+++ Development/Sprint-3/Code/odysseum-admin-frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { Button } from "antd";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -16,11 +17,11 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-5xl mt-5 font-bold">Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <Button variant="dashed" color="pink" onClick={() => setCount((count) => count + 1)}>
+          This is an Ant Design Button.count is {count}
+        </Button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -29,7 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
