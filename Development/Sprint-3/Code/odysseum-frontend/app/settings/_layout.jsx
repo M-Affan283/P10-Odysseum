59a78d352650ec803b0e28b0d4d091d8ee7f1d85
Muhammad Affan <25100283@lums.edu.pk>
2025-03-09 22:13:35 +0500

    New settings pages and UI

--- Development/Sprint-3/Code/odysseum-frontend/app/settings/_layout.jsx
+++ Development/Sprint-3/Code/odysseum-frontend/app/settings/_layout.jsx
@@ -11,6 +11,11 @@ const SettingsLayout = () => {
         <Stack.Screen name="business/index" options={{ headerShown: false }} />
         <Stack.Screen name="business/create" options={{ headerShown: false }} />
         <Stack.Screen name="business/manage" options={{ headerShown: false }} />
+        <Stack.Screen name="service/create/[createId]" options={{ headerShown: false }} />
+        <Stack.Screen name="service/manage/[manageId]" options={{ headerShown: false }} />
+        <Stack.Screen name="profile/index" options={{ headerShown: false }} />
+        <Stack.Screen name="profile/manage" options={{ headerShown: false }} />
+        <Stack.Screen name="profile/password" options={{ headerShown: false }} />
       </Stack>
     </>
   );
