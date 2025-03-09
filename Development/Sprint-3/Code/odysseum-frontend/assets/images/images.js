ee2f7010e444f55db3cb32f80204a86762f72853
Muhammad Affan <25100283@lums.edu.pk>
2025-03-09 22:05:41 +0500

    New assets

--- Development/Sprint-3/Code/odysseum-frontend/assets/images/images.js
+++ Development/Sprint-3/Code/odysseum-frontend/assets/images/images.js
@@ -11,6 +11,14 @@ import BusinessSearchImg from './BusinessSearch.jpg';
 import UserSearchImg from './FindUser.jpg';
 import DiscoverLocationImg from './DiscoverLocation.jpg';
 import DefaultLocationImg from './Sunset.jpg';
+import CreateServiceImg from './CreateService.jpg';
+import ServicePricingImg from './ServicePricing.jpg';
+import PaymentImg from './Payment.jpg';
+import BookingImg from './Booking.jpg';
+import CancelImg from './Cancel.jpg';
+import AvailablityImg from './Availability.jpg';
+import CustomDetailsImg from './CustomDetails.jpg';
+import ActivityImg from './Activity.jpg';
 
 const images = {
     CreateBusinessImg,
@@ -25,7 +33,15 @@ const images = {
     BusinessSearchImg,
     UserSearchImg,
     DiscoverLocationImg,
-    DefaultLocationImg
+    DefaultLocationImg,
+    CreateServiceImg,
+    ServicePricingImg,
+    PaymentImg,
+    BookingImg,
+    CancelImg,
+    AvailablityImg,
+    CustomDetailsImg,
+    ActivityImg
 };
 
 export default images;
