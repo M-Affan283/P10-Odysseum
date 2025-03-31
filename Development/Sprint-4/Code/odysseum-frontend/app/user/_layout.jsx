76173f59357833cabd9fd32a0423145b2c07ec57
Muhammad Affan <25100283@lums.edu.pk>
2025-03-31 19:56:18 +0500

    Fixed layout routing

--- Development/Sprint-4/Code/odysseum-frontend/app/user/_layout.jsx
+++ Development/Sprint-4/Code/odysseum-frontend/app/user/_layout.jsx
@@ -9,6 +9,8 @@ const UserLayout = () => {
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="[id]" options={{ headerShown: false }} />
+        <Stack.Screen name="booking/bookings" options={{ headerShown: false }} />
+        <Stack.Screen name="booking/[id]" options={{ headerShown: false }} />
       </Stack>
     </>
   );
